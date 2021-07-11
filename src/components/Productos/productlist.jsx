fdf1ef9154af998a6e70bded3efcbe1167b530c5
juanmgarcia21 <juanmgv21@gmail.com>
2021-07-11 10:35:59 -0300

    entrega 6

--- src/components/Productos/productlist.jsx
+++ src/components/Productos/productlist.jsx
@@ -1,35 +1,29 @@
 const productList = [
 
-    {
-        id: 1,
-        title: "RED HOT CHILLI PEPPERS - One Hot Minute",
-        price: 6000,
-        img: "./img/redhot.jpg"
-    },
 
     {
-        id: 2,
+        id: 1,
         title: "LANA DEL REY - Chemtrails Over The Country Club",
         price: 4800,
         img: "./img/lanadelrey.jpg"
     },
 
     {
-        id: 3,
+        id: 2,
         title: "LADY GAGA - Chromatica",
         price: 3500,
         img: "./img/ladygaga.jpg"
     },
 
     {
-        id: 4,
+        id: 3,
         title: "THE KILLERS - Imploding The Mirage",
         price: 4200,
         img: "./img/thekillers.jpg"
     },
 
     {
-        id: 5,
+        id: 4,
         title: "MICHAEL JACKSON - Dangerous",
         price: 5700,
         img: "./img/dangerous.jpg"
